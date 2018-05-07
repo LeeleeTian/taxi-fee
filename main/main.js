7beaebc0df6f5fc6e3e786f5b002b04afc63589b
Leelee Tian <tianlihang2013@gmail.com>
2018-05-07 17:02:34 +0800

    Complete taxi-fee

--- main/main.js
+++ main/main.js
@@ -1,4 +1,23 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+'use strict'
+
+var split = function(drivingString) {
+  let arr = drivingString.match(/\d+(\.\d+)?/g).map((m) => parseFloat(m))
+  return {distance: arr[0], waitingTime: arr[1]};
+};
+
+var computePrice = function(drivingInfo) {
+  return Math.round(drivingInfo.waitingTime * 0.25 +
+  ((drivingInfo.distance > 8)? 10 + (drivingInfo.distance - 8) * 1.2
+  :(drivingInfo.distance > 2)? 6 + (drivingInfo.distance - 2) * 0.8
+  : (drivingInfo.distance > 0)? 6 : 0));
+}
+
+var print = function(price) {
+  return '一共' + price + '元';
+}
+
+module.exports = function main(drivingString) {
+    var drivingInfo = split(drivingString);
+    var price = computePrice(drivingInfo);
+    return print(price);
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,26 @@
+// 'use strict'
+
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    // write your tests here...
+    it('return 0 yuan given 0 km and 0 min',function () {
+      var result = main('行驶0公里, 等待0分钟');
+      expect(result).toEqual('一共0元');
+    });
+    it('return 6 yuan given 2 km and 0 min',function () {
+      var result = main('行驶2公里, 等待0分钟');
+      expect(result).toEqual('一共6元');
+    });
+    it('return 9 yuan given 5.5 km and 0 min',function () {
+      var result = main('行驶5.5公里, 等待0分钟');
+      expect(result).toEqual('一共9元');
+    });
+    it('return 19 yuan given 15.6 km and 0 min',function () {
+      var result = main('行驶15.6公里, 等待0分钟');
+      expect(result).toEqual('一共19元');
+    });
+    it('return 29 yuan given 22.3 km and 5.5 min',function () {
+      var result = main('行驶22.3公里, 等待5.5分钟');
+      expect(result).toEqual('一共29元');
+    });
 });
